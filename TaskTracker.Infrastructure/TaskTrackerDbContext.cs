f4e31ebc56fe5d84f39e6b4a3498157305ece58f
f14shm4n <uraborg@gmail.com>
2025-09-10 00:44:36 +0300

    add: created base project parts.

---
+++ TaskTracker.Infrastructure/TaskTrackerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker.Infrastructure
+{
+    public sealed class TaskTrackerDbContext : DbContext
+    {
+        public TaskTrackerDbContext(DbContextOptions<TaskTrackerDbContext> options) : base(options)
+        {
+        }
+    }
+}
