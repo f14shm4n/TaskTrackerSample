4b69e1c7722658b1b644462b51e58adcfcd211f3
f14shm4n <uraborg@gmail.com>
2025-09-11 01:41:52 +0300

    up: split classes.

--- TaskTracker.API/Application/Commands/ClearMasterTaskCommand.cs
+++ TaskTracker.API/Application/Commands/ClearMasterTaskCommand.cs
@@ -1,6 +1,5 @@
 ﻿using MediatR;
 using System.ComponentModel.DataAnnotations;
-using TaskTracker.Domain.Aggregates.Tasks;
 
 namespace TaskTracker.API.Application.Commands
 {
@@ -11,35 +10,4 @@ namespace TaskTracker.API.Application.Commands
     }
 
     public record ClearMasterTaskCommandResponse(bool Success) : ApiResponseBase(Success);
-
-    public class ClearMasterTaskCommandHandler : IRequestHandler<ClearMasterTaskCommand, ClearMasterTaskCommandResponse>
-    {
-        private readonly ILogger<ClearMasterTaskCommandHandler> _logger;
-        private readonly ITaskRepository _taskRepository;
-
-        public ClearMasterTaskCommandHandler(ILogger<ClearMasterTaskCommandHandler> logger, ITaskRepository taskRepository)
-        {
-            _logger = logger;
-            _taskRepository = taskRepository;
-        }
-
-        public async Task<ClearMasterTaskCommandResponse> Handle(ClearMasterTaskCommand request, CancellationToken cancellationToken)
-        {
-            try
-            {
-                var subTask = await _taskRepository.GetAsync(request.TaskId, cancellationToken);
-                if (subTask != null)
-                {
-                    subTask.RemoveMasterTask();
-                    var r = await _taskRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
-                    return new ClearMasterTaskCommandResponse(r > 0);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Unable to remove as sub task. TaskId: '{TaskId}'.", request.TaskId);
-            }
-            return new ClearMasterTaskCommandResponse(false);
-        }
-    }
 }
