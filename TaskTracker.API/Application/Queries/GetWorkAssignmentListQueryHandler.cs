1887bcec959aca45e9518be4a1c1d5f29a791a83
f14shm4n <uraborg@gmail.com>
2025-09-11 22:02:49 +0300

    refactor: split classes.

---
+++ TaskTracker.API/Application/Queries/GetWorkAssignmentListQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using TaskTracker.API.Application.Extensions;
+using TaskTracker.Domain.Aggregates.WorkAssignment;
+
+namespace TaskTracker.API.Application.Queries
+{
+    public class GetWorkAssignmentListQueryHandler : IRequestHandler<GetWorkAssignmentListQuery, GetWorkAssignmentListQueryResponse>
+    {
+        private readonly ILogger<GetWorkAssignmentListQueryHandler> _logger;
+        private readonly IWorkAssignmentRepository _workRepository;
+
+        public GetWorkAssignmentListQueryHandler(ILogger<GetWorkAssignmentListQueryHandler> logger, IWorkAssignmentRepository workRepository)
+        {
+            _logger = logger;
+            _workRepository = workRepository;
+        }
+
+        public async Task<GetWorkAssignmentListQueryResponse> Handle(GetWorkAssignmentListQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                IEnumerable<WorkAssignment> collection;
+                if (request.WithRelatedData)
+                {
+                    collection = await _workRepository.GetCollectionWithIncludesAsync(request.Offset, request.Limit, request.OnlyHeadTasks, cancellationToken);
+                }
+                else
+                {
+                    collection = await _workRepository.GetCollectionAsync(request.Offset, request.Limit, request.OnlyHeadTasks, cancellationToken);
+                }
+                return new GetWorkAssignmentListQueryResponse(collection.Select(x => x.ToWorkAssignmentDto()).ToList());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unable to get work assignment list.");
+            }
+            return new GetWorkAssignmentListQueryResponse(null);
+        }
+    }
+}
